2b376fb2cdf25fa657ce8af9f023bbd43784b43c
medofrh <info@medo98.com>
2024-11-29 10:48:00 +0100

    init ClassRoom class

--- src/main/java/org/sulaiman/ClassRoom.java
+++ src/main/java/org/sulaiman/ClassRoom.java
@@ -1,2 +1,27 @@
-package org.sulaiman;public class ClassRoom {
+package org.sulaiman;
+
+public class ClassRoom {
+    private int classRoomId;
+    private String name;
+
+    public ClassRoom(int classRoomId, String name) {
+        this.classRoomId = classRoomId;
+        this.name = name;
+    }
+
+    public int getClassRoomId() {
+        return classRoomId;
+    }
+
+    public void setClassRoomId(int classRoomId) {
+        this.classRoomId = classRoomId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
