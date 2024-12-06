aa271c4e8acc87b440e2fa8a01a19311b7a8b870
medofrh <info@medo98.com>
2024-12-06 21:54:55 +0100

    Add App and DatabaseList classes; remove GradeFactory class

--- src/main/java/org/sulaiman/GradeFactory.java
+++
@@ -1,7 +0,0 @@
-package org.sulaiman;
-
-public class GradeFactory {
-    public static Grade createGrade(Subject subject, Student student, int score){
-        return new Grade(student, subject, score);
-    }
-}
