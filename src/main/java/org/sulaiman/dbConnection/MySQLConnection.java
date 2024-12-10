a193b8a158a28ef8589d325b12703ebbd70efc9d
medofrh <info@medo98.com>
2024-12-10 14:46:00 +0100

    Refactor MySQLConnection class to use static fields and methods for improved connection handling

--- src/main/java/org/sulaiman/dbConnection/MySQLConnection.java
+++ src/main/java/org/sulaiman/dbConnection/MySQLConnection.java
@@ -4,24 +4,24 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.*;
 public class MySQLConnection {
-    private final Dotenv dotenv = Dotenv.load();
-    private final String username = dotenv.get("DB_USER");
-    private final String password = dotenv.get("DB_PASS");
-    private final String host = dotenv.get("DB_HOST");
-    private final String database = dotenv.get("DB_NAME");
-    private final int port = Integer.parseInt(dotenv.get("DB_PORT"));
+    private final static Dotenv dotenv = Dotenv.load();
+    private final static String username = dotenv.get("DB_USER");
+    private final static String password = dotenv.get("DB_PASS");
+    private final static String host = dotenv.get("DB_HOST");
+    private final static String database = dotenv.get("DB_NAME");
+    private final static int port = Integer.parseInt(dotenv.get("DB_PORT"));
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         try {
-            String mysqlUrl = "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database +
+            String mysqlUrl = "jdbc:mysql://" + host + ":" + port + "/" + database +
                     "?useSSL=false&serverTimezone=UTC";
-            return DriverManager.getConnection(mysqlUrl, this.username, this.password);
+            return DriverManager.getConnection(mysqlUrl, username, password);
         }catch (SQLException e){
             return null;
         }
     }
 
-    public boolean testConnection() {
+    public static boolean testConnection() {
         try (Connection conn = getConnection()) {
             return conn != null;
         } catch (SQLException e) {
