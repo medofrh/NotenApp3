fec4b61e4cf9425ab71be8b396d2595e5c08ab7d
medofrh <info@medo98.com>
2024-12-10 15:15:05 +0100

    Implement DatabaseManager to handle database connections and execute SELECT queries

--- src/main/java/org/sulaiman/DatabaseManager.java
+++ src/main/java/org/sulaiman/DatabaseManager.java
@@ -1,2 +1,28 @@
-package org.sulaiman;public class DatabaseManager {
+package org.sulaiman;
+
+import org.sulaiman.dbConnection.MySQLConnection;
+import org.sulaiman.dbConnection.SQLiteConnection;
+
+import java.sql.*;
+
+public class DatabaseManager {
+    private static Connection connection;
+
+    public DatabaseManager() {
+        if (MySQLConnection.testConnection()) {
+            connection = MySQLConnection.getConnection();
+        }else {
+            connection = SQLiteConnection.getConnection();
+        }
+    }
+
+    // select method for MySQL and SQLite databases
+    public ResultSet select(String sql) throws SQLException {
+        //filter the sql query to determine if it's a select query
+        if (!sql.trim().toLowerCase().startsWith("select")) {
+            throw new SQLException("Invalid query, only SELECT queries are allowed.");
+        }
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        return pstmt.executeQuery();
+    }
 }
