792d6735b64629194cde1b6811cdff9cf0a89a15
medofrh <info@medo98.com>
2024-12-15 04:48:59 +0100

    Remove unused imports in Main.java for cleaner code

--- src/main/java/org/sulaiman/Main.java
+++ src/main/java/org/sulaiman/Main.java
@@ -3,10 +3,6 @@ package org.sulaiman;
 import org.sulaiman.dbConnection.SQLiteConnection;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
