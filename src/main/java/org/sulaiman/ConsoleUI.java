bb79bc6d9982177b6f253d5dc45cda2aca13b457
medofrh <info@medo98.com>
2024-12-17 15:05:24 +0100

    Refactor ConsoleUI to return AbstractUser and implement subject retrieval methods for Student and Teacher

--- src/main/java/org/sulaiman/ConsoleUI.java
+++ src/main/java/org/sulaiman/ConsoleUI.java
@@ -2,15 +2,15 @@ package org.sulaiman;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConsoleUI {
     private static boolean isRunning = true;
 
     // Start the console UI
-    public static Object start() {
+    public static AbstractUser start() {
         Scanner scanner = new Scanner(System.in);
-        Object user = null;
 
         while (isRunning) {
             displayMainMenu();
@@ -20,7 +20,7 @@ public class ConsoleUI {
                 case "1":
                     // Login
                     clearScreen();
-                    user = login(scanner);
+                    AbstractUser user = login(scanner);
                     if (user != null) {
                         return user;
                     }
@@ -92,11 +92,11 @@ public class ConsoleUI {
         }
     }
 
-    public static Object login(Scanner scanner) {
+    public static AbstractUser login(Scanner scanner) {
         // Login
         clearScreen();
         try {
-            DatabaseManager dbManager = new DatabaseManager();
+            DatabaseManager dbManager = DatabaseManager.getInstance();
             System.out.println("==============Login==============");
             System.out.println("1. Student");
             System.out.println("2. Teacher");
@@ -174,5 +174,167 @@ public class ConsoleUI {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public static ArrayList<Subject> getSubjects(AbstractUser user) {
+        if (user instanceof Student) {
+            return getSubjects((Student) user);
+        } else if (user instanceof Teacher) {
+            return getSubjects((Teacher) user);
+        }
+        return new ArrayList<>();
+    }
+
+    public static ArrayList<Subject> getSubjects(Student student) {
+        ArrayList<Subject> subjects = new ArrayList<>();
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT * FROM student_subject WHERE student_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, student.getUid());
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                subjects.add(ConsoleUI.getSubject(resultSet.getInt("subject_id")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return subjects;
+    }
+
+    public static ArrayList<Subject> getSubjects(Teacher teacher) {
+        ArrayList<Subject> subjects = new ArrayList<>();
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT subject_id FROM subject WHERE teacher_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, teacher.getUid());
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                subjects.add(ConsoleUI.getSubject(resultSet.getInt("subject_id")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return subjects;
+    }
+
+    public static Subject getSubject(int subjectId) {
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT * FROM subject WHERE subject_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, subjectId);
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                return new Subject(
+                        resultSet.getInt("subject_id"),
+                        resultSet.getString("name"),
+                        ConsoleUI.getClassRoom(resultSet.getInt("classroom_id")),
+                        ConsoleUI.getTeacher(resultSet.getInt("teacher_id"))
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Teacher getTeacher(int teacherId) {
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT * FROM teacher WHERE teacher_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, teacherId);
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                return new Teacher(
+                        resultSet.getInt("teacher_id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("email")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ClassRoom getClassRoom(int classRoomId) {
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT * FROM classroom WHERE classroom_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, classRoomId);
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                return new ClassRoom(
+                        resultSet.getInt("classroom_id"),
+                        resultSet.getString("name")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<Student> getStudents(Subject subject) {
+        ArrayList<Student> students = new ArrayList<>();
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT student_id FROM student_subject WHERE subject_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, subject.getSubjectId());
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                students.add(ConsoleUI.getStudent(resultSet.getInt("student_id")));
+            }
+            return students;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static Student getStudent(int studentId) {
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+
+        // read mm relations
+        String query = "SELECT * FROM student WHERE student_id = ?";
+        try {
+            PreparedStatement stmt = dbManager.getConnection().prepareStatement(query);
+            stmt.setInt(1, studentId);
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                return new Student(
+                        resultSet.getInt("student_id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("email")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
