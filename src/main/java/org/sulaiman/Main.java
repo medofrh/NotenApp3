fd34fa059d9c25a0ee2ce60f0106308769e73412
medofrh <info@medo98.com>
2024-12-18 02:37:17 +0100

    Remove commented-out code in Main class

--- src/main/java/org/sulaiman/Main.java
+++ src/main/java/org/sulaiman/Main.java
@@ -1,10 +1,7 @@
 package org.sulaiman;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-//        new App();
         DatabaseManager.getInstance();
         ConsoleUI.start();
     }
